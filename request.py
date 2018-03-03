950dbc6261d2e6f8baa6cd63130cc1b7724368c1
aalok.sathe <aalok.sathe@gmail.com>
2018-03-03 11:14:17 -0500

    add timestamp to request

--- request.py
+++ request.py
@@ -1,6 +1,11 @@
+import time
+
 class Request: 
+
+    keywords = list()
+    timestamp = None
   
   def __init__(self, stringMessage):
     self.message = stringMessage
 
-
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
