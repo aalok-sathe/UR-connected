5fa79a6fa41c5e00b888816dfff5561f97dc9d3d
aalok.sathe <aalok.sathe@gmail.com>
2018-03-03 13:48:06 -0500

    renaming stuff

---
+++ event.py
@@ -0,0 +1,16 @@
+import time
+
+class Request: 
+
+    keywords = list()
+    timestamp = None
+    seconds = None
+    message = ""
+    location = ""
+  
+    def __init__(self, stringMessage, stringLocation):
+        self.message = stringMessage
+        self.location = stringLocation
+        self.seconds = time.time()
+        self.timestamp = datetime.datetime.fromtimestamp(self.seconds).strftime('%Y-%m-%d %H:%M:%S')
+
