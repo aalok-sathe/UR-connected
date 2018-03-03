7e4674cf7b018669dfd959c834dc80a6929c2cc5
aalok.sathe <aalok.sathe@gmail.com>
2018-03-03 11:32:05 -0500

    add PQ

--- forum.py
+++ forum.py
@@ -2,27 +2,26 @@
 
 from request import Request
 import pickle
-
+from tkinter import *
 
 class FacilitiesForum(Cmd):
     
-        
-    
+    request_queue = []
+     
     def __init__(self):
             
-        
-    
+               
         
 if __name__ == '__main__':
     prompt = FacilitiesForum("")
-    prompt.prompt = ':: '
+    prompt.prompt = 'UR Repaired! '
     try:
     	prompt.cmdloop('''
-    (C) 2018 Ting C., Hammad H., Yanish R., Aalok S.
+    (C) Copyright 2018. Ting C., Hammad H., Yanish R., Aalok S.
     This program comes with ABSOLUTELY NO WARRANTY.
     This is free software, and you are welcome to redistribute it
     under certain conditions. See the GNU LGPL 3 (The LESSER GPL).
     ''')
-    except KeyboardInterrupt :
+    except KeyboardInterrupt:
     	print("\nExiting due to KeyboardInterrupt")
     	raise SystemExit
