461aebbf3e33baf11cebedc192560a86aaaba0e3
Ting Chen <tingtang4170@gmail.com>
2018-03-03 11:03:56 -0500

    Yeet
    Fixed

--- request.py
+++ request.py
@@ -1,7 +1,6 @@
 class Request: 
   def __init__(self, stringMessage):
     self.message = stringMessage
-#yeeet<<<<<<< HEAD
+<<<<<<< HEAD
 =======
-#I like boys
 >>>>>>> 7298110e295c3005408a27a2b0de64fc78adaf79
