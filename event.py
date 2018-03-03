3b50d3d125a67ac9951f19a8bb5da46155db746a
aalok.sathe <aalok.sathe@gmail.com>
2018-03-03 15:44:05 -0500

    add data fields to '

--- event.py
+++ event.py
@@ -5,12 +5,18 @@ class Event:
     keywords = list()
     timestamp = None
     seconds = None
-    message = ""
-    location = ""
+    body = None
+    title = None
+    location = None
+    
+    attendees = list()
+    thumbs = list((0,0))
+    expiry = None
   
-    def __init__(self, stringMessage, stringLocation):
-        self.message = stringMessage
-        self.location = stringLocation
+    def __init__(self, title=None, body=None, location=None, user=None, keywords_dict=None):
+        self.title = str(title)
+        self.body = str(body)
+        self.location = str(location)
         self.seconds = time.time()
         self.timestamp = datetime.datetime.fromtimestamp(self.seconds).strftime('%Y-%m-%d %H:%M:%S')
-
+        
