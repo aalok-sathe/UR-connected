9a26bb7d35cc139ce94ee55d2716d1dd67976107
Yanish Ramboucs <yanishrambocus@gmail.com>
2018-03-03 17:18:21 -0500

    WEB

--- application.py
+++ application.py
@@ -1,26 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, session, url_for, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def main():
 	#return render_template('Home.html')
 	return render_template('index.html')
-@app.route("/register", methods=["GET", "POST"])
+
+@app.route("/register.html", methods=["GET", "POST"])
 def register():
 	#Register user - puts data in users.csv
-		if request.method == "POST":
-			users = open("users.csv", a)
-			users.write(session["email"], session["password"])
-		else:
-			return render_template("register.html")
+	if request.method == "POST     ":
+		main()
+		users = open("users.csv", a)
+		users.write(session["email"], session["password"])
+		Home()
+	else:
+		return render_template('register.html')
 
 
 
-@app.route("/login", methods =["GET", "POST"])
+@app.route("/login.html", methods =["GET", "POST"])
 def login():
 		#Log user in with valid login credentials
 	return None
 
+@app.route("/Home.html", methods = ["GET"])
+def Home():
+	return render_template('Home.html')
+
+
 if __name__ == "__main__":
      app.run()
+
